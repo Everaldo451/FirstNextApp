069ea9180cbd8e430be28c31c95fd13bc33f16b1
Everaldo451 <everaldo.work.jr@gmail.com>
2025-01-15 23:26:33 -0300

    JWT changes

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/db";
 import { hashData } from "@/lib/encryptData";
+import { cookies } from "next/headers";
 import createJsonResponse from "@/lib/createResponse";
 import loginUser from "@/lib/loginUser";
+import jwtServiceInstance from "@/lib/jwtService";
 
 export default async function POST(request:NextRequest){
     const {password, email} = await request.json()
@@ -16,6 +18,11 @@ export default async function POST(request:NextRequest){
         if (!user) {
             return createJsonResponse("Login unauthorized.", 401)
         }
+
+        const cookieStore = await cookies()
+        jwtServiceInstance.setAccessCookie(user.id, cookieStore)
+        jwtServiceInstance.setRefreshCookie(user.id, cookieStore)
+        
         return createJsonResponse("User login did successfull.",200)
     }catch(error) {
         return createJsonResponse("Internal server Error.",500)
